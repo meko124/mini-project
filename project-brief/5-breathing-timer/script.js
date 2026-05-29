e977f216ec4610491822a998bb72fc6fd67f0727
MUHAMMAD EKO KUSWANTO <mekokuswanto123@gmail.com>
2026-05-29 22:33:20 +0700

    add line and change code

---
+++ project-brief/5-breathing-timer/script.js
@@ -0,0 +1,6 @@
+const phases = [
+	{label: 'breath in', duration: 4},
+	{label: 'hold your breath', duration: 4},
+	{label: 'breath out', duration: 6},
+	{label: 'hold your breath', duration: 2},
+];
