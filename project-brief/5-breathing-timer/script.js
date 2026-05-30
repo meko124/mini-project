c92cd8e813198906a3164e787c7bcde361f1b83b
MUHAMMAD EKO KUSWANTO <mekokuswanto123@gmail.com>
2026-05-30 22:22:23 +0700

    add line code

--- project-brief/5-breathing-timer/script.js
+++ project-brief/5-breathing-timer/script.js
@@ -1,6 +1,24 @@
+
 const phases = [
-	{label: 'breath in', duration: 4},
-	{label: 'hold your breath', duration: 4},
-	{label: 'breath out', duration: 6},
-	{label: 'hold your breath', duration: 2},
+	{label: 'breath in', count: 4},
+	{label: 'hold your breath', count: 4},
+	{label: 'breath out', count: 6},
+	{label: 'hold your breath', count: 2},
 ];
+
+let timer = null;
+let phaseIdx = 0;
+let countdown = phases[0].count;
+
+function start() {
+	timer = setInterval(() => {
+
+			document.getElementById("count").textContent = countdown;
+			document.getElementById("label").textContent = phases[phaseIdx].label;
+
+			countdown--;
+	}, 1000);
+}
+function stop() {
+	clearInterval(timer);
+}
