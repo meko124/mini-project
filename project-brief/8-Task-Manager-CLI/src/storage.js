64567b6895f35007dcd7d8029e468d955d1030f8
Muhammad Eko Kuswanto <mekokuswanto123@gmail.com>
2026-09-01 22:25:57 +0700

    add line code in storage.js

--- project-brief/8-Task-Manager-CLI/src/storage.js
+++ project-brief/8-Task-Manager-CLI/src/storage.js
@@ -1,2 +1,12 @@
-const fs = require('node:fs')
-const path = require('./data/tasks.json');
+const fs = require('node:fs');
+const path = require('node:path');
+const filePath = path.join(__dirname,'..','data','tasks.json');
+
+async function readTasks() {
+	try {
+		const data = await fs.promises.readFile(filePath,'utf8');
+		return JSON.parse(data);
+	} catch {
+		return [];
+	}
+}
