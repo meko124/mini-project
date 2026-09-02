7cead65e7f847f5b1cde259a1c7bc24fc5d6b5d7
Muhammad Eko Kuswanto <mekokuswanto123@gmail.com>
2026-09-02 22:10:53 +0700

    add line code in storage.js

--- project-brief/8-Task-Manager-CLI/src/storage.js
+++ project-brief/8-Task-Manager-CLI/src/storage.js
@@ -1,12 +1,19 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const filePath = path.join(__dirname,'..','data','tasks.json');
+const filePath = path.join(__dirname,'..','data','tasks.json');//note:lokasi direktory
 
 async function readTasks() {
 	try {
-		const data = await fs.promises.readFile(filePath,'utf8');
-		return JSON.parse(data);
+		const data = await fs.promises.readFile(filePath,{encoding:'utf8'}); //note: Tunggu sampai selesai — minta Node.js bacakan file di lokasi ini, menggunakan bahasa utf8
+		return JSON.parse(data);//note:"Ubah teks mentah (data) menjadi object JavaScript lalu kembalikan hasilnya ke yang memanggil function ini"
 	} catch {
 		return [];
 	}
 }
+
+async function writeTasks(tasks) {
+	const data = JSON.stringify(tasks,null,2);
+	await fs.promises.writeFile(filePath,tasks,{encoding:'utf8'});
+}
+
+module.exports = {readTasks,writeTasks}
